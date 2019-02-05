91c04c075330c17dee4398a7d16d2b49210ce233
Daniël Klabbers <daniel@klabbers.email>
2019-02-05 13:17:10 +0100

    horizon service provider

--- src/Providers/HorizonServiceProvider.php
+++ src/Providers/HorizonServiceProvider.php
@@ -2,17 +2,36 @@
 
 namespace Bokt\Horizon\Providers;
 
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\View\Factory;
 use Laravel\Horizon\HorizonServiceProvider as Provider;
 
 class HorizonServiceProvider extends Provider
 {
-    protected function registerRoutes()
+    public function register()
     {
 
     }
 
+    protected function registerRoutes()
+    {
+        // .. via extend.php
+    }
+
     protected function registerResources()
     {
+        /** @var Factory $view */
+        $view = $this->app->make(Factory::class);
 
+        $view->addNamespace('horizon', base_path('vendor/laravel/horizon/resources/views'));
+    }
+
+    protected function configure()
+    {
+        $config = include base_path('vendor/laravel/horizon/config/horizon.php');
+
+        /** @var Repository $repository */
+        $repository = $this->app->make(Repository::class);
+        $repository->set(['horizon' => $config]);
     }
 }
