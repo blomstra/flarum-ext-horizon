ac4248419e03ebe9d8b7f317c88a4959c7d1d0f7
Daniël Klabbers <daniel@klabbers.email>
2021-01-27 16:49:21 +0100

    add override work command

---
+++ src/Console/WorkCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bokt\Horizon\Console;
+
+use Flarum\Foundation\Config;
+
+class WorkCommand extends \Laravel\Horizon\Console\WorkCommand
+{
+    protected function downForMaintenance()
+    {
+        if ($this->option('force')) {
+            return false;
+        }
+
+        /** @var Config $config */
+        $config = $this->laravel->make(Config::class);
+
+        return $config->inMaintenanceMode();
+    }
+}
