94569ace124c37c550d39f54916c1b99ffe1ae07
Ian Morland <ian@morland.me>
2022-07-15 12:45:14 +0100

    wip

--- src/Http/Home.php
+++ src/Http/Home.php
@@ -12,12 +12,14 @@
 
 namespace Blomstra\Horizon\Http;
 
+use Flarum\Foundation\Config;
 use Flarum\Frontend\Frontend;
 use Flarum\Http\UrlGenerator;
 use Illuminate\Contracts\Filesystem\Factory as FilesystemFactory;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\View\Factory;
 use Laminas\Diactoros\Response\HtmlResponse;
+use Laravel\Horizon\Horizon;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -43,21 +45,25 @@ class Home implements RequestHandlerInterface
      */
     private $url;
 
-    public function __construct(Factory $view, Frontend $frontend, FilesystemFactory $filesystem, UrlGenerator $url)
+    /** @var Config */
+    private $config;
+
+    public function __construct(Factory $view, Frontend $frontend, FilesystemFactory $filesystem, UrlGenerator $url, Config $config)
     {
         $this->view = $view;
         $this->frontend = $frontend;
         $this->assetsDir = $filesystem->disk('flarum-assets');
         $this->url = $url;
+        $this->config = $config;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         return new HtmlResponse($this->view->make('horizon::layout', [
-            'cssFile'                => 'app.css',
-            'horizonScriptVariables' => [
-                'path' => 'admin/horizon',
-            ],
+            'assetsAreCurrent' => $this->config->inDebugMode(),
+            'cssFile' => 'app.css', // TODO: support fof/nightmode
+            'horizonScriptVariables' => Horizon::scriptVariables(),
+            'isDownForMaintenance' => $this->config->inMaintenanceMode(),
             'assetsUrl'                    => $this->assetsDir->url('/'),
         ])->render());
     }
