45eabb5c8394f2dd5df006f97f8243f0d295b0e8
Ian Morland <16573496+imorland@users.noreply.github.com>
2022-07-27 17:45:03 +0100

    feat: Restart the queue (#8)
    * Add queue restarter ahead of Flarum 1.5
    
    * Apply fixes from StyleCI
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ src/QueueRestarter.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of blomstra/horizon.
+ *
+ * Copyright (c) Bokt.
+ * Copyright (c) Blomstra Ltd.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Blomstra\Horizon;
+
+use Flarum\Extension\Event\Disabled;
+use Flarum\Extension\Event\Enabled;
+use Flarum\Foundation\Event\ClearingCache;
+use Flarum\Settings\Event\Saved;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Queue\Console\RestartCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+/**
+ * Required only until Flarum 1.5 - https://github.com/flarum/framework/pull/3565.
+ */
+class QueueRestarter
+{
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * @var RestartCommand
+     */
+    protected $command;
+
+    public function __construct(Container $container, RestartCommand $command)
+    {
+        $this->container = $container;
+        $this->command = $command;
+    }
+
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen([
+            ClearingCache::class, Saved::class,
+            Enabled::class, Disabled::class,
+        ], [$this, 'restart']);
+    }
+
+    public function restart()
+    {
+        $this->command->setLaravel($this->container);
+
+        $this->command->run(
+            new ArrayInput([]),
+            new NullOutput()
+        );
+    }
+}
